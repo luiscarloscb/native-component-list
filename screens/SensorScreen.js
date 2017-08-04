7706c43b656a37bc7422ec5609a393fbac301024
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-08-04 21:27:29 +0000

    Add Magnetometer module (#385)
    fbshipit-source-id: 805b6be

--- screens/SensorScreen.js
+++ screens/SensorScreen.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { Accelerometer, Gyroscope } from 'expo';
+import {
+  Accelerometer,
+  Gyroscope,
+  Magnetometer,
+  MagnetometerUncalibrated,
+} from 'expo';
 
 export default class SensorScreen extends React.Component {
   static navigationOptions = {
@@ -13,142 +18,84 @@ export default class SensorScreen extends React.Component {
       <View style={styles.container}>
         <GyroscopeSensor />
         <AccelerometerSensor />
+        <MagnetometerSensor />
+        <MagnetometerUncalibratedSensor />
       </View>
     );
   }
 }
 
-class GyroscopeSensor extends React.Component {
-  state = {
-    gyroscopeData: {},
-  };
+function createSensorBlock(name, Sensor) {
+  return class SensorBlock extends React.Component {
+    state = {
+      data: {},
+    };
 
-  componentDidMount() {
-    this._toggle();
-  }
-
-  componentWillUnmount() {
-    this._unsubscribe();
-  }
-
-  _toggle = () => {
-    if (this._subscription) {
+    componentWillUnmount() {
       this._unsubscribe();
-    } else {
-      this._subscribe();
+    }
+
+    _toggle = () => {
+      if (this._subscription) {
+        this._unsubscribe();
+      } else {
+        this._subscribe();
+      }
+    };
+
+    _slow = () => {
+      Sensor.setUpdateInterval(1000);
+    };
+
+    _fast = () => {
+      Sensor.setUpdateInterval(16);
+    };
+
+    _subscribe = () => {
+      this._subscription = Sensor.addListener(result => {
+        this.setState({ data: result });
+      });
+    };
+
+    _unsubscribe = () => {
+      this._subscription && this._subscription.remove();
+      this._subscription = null;
+    };
+
+    render() {
+      let { x, y, z } = this.state.data;
+
+      return (
+        <View style={styles.sensor}>
+          <Text>{name}:</Text>
+          <Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity onPress={this._toggle} style={styles.button}>
+              <Text>Toggle</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={this._slow}
+              style={[styles.button, styles.middleButton]}>
+              <Text>Slow</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this._fast} style={styles.button}>
+              <Text>Fast</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      );
     }
   };
-
-  _slow = () => {
-    Gyroscope.setUpdateInterval(1000);
-  };
-
-  _fast = () => {
-    Gyroscope.setUpdateInterval(16);
-  };
-
-  _subscribe = () => {
-    this._subscription = Gyroscope.addListener(result => {
-      this.setState({ gyroscopeData: result });
-    });
-  };
-
-  _unsubscribe = () => {
-    this._subscription && this._subscription.remove();
-    this._subscription = null;
-  };
-
-  render() {
-    let { x, y, z } = this.state.gyroscopeData;
-
-    return (
-      <View style={styles.sensor}>
-        <Text>Gyroscope:</Text>
-        <Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
-
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={this._toggle} style={styles.button}>
-            <Text>Toggle</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={this._slow}
-            style={[styles.button, styles.middleButton]}>
-            <Text>Slow</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this._fast} style={styles.button}>
-            <Text>Fast</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
 }
 
-class AccelerometerSensor extends React.Component {
-  state = {
-    accelerometerData: {},
-  };
-
-  componentDidMount() {
-    this._toggle();
-  }
-
-  componentWillUnmount() {
-    this._unsubscribe();
-  }
-
-  _toggle = () => {
-    if (this._subscription) {
-      this._unsubscribe();
-    } else {
-      this._subscribe();
-    }
-  };
-
-  _slow = () => {
-    Accelerometer.setUpdateInterval(1000);
-  };
-
-  _fast = () => {
-    Accelerometer.setUpdateInterval(16);
-  };
-
-  _subscribe = () => {
-    this._subscription = Accelerometer.addListener(result => {
-      this.setState({ accelerometerData: result });
-    });
-  };
-
-  _unsubscribe = () => {
-    this._subscription && this._subscription.remove();
-    this._subscription = null;
-  };
-
-  render() {
-    let { x, y, z } = this.state.accelerometerData;
-
-    return (
-      <View style={styles.sensor}>
-        <Text>Accelerometer:</Text>
-        <Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
-
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={this._toggle} style={styles.button}>
-            <Text>Toggle</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={this._slow}
-            style={[styles.button, styles.middleButton]}>
-            <Text>Slow</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this._fast} style={styles.button}>
-            <Text>Fast</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
-}
+const GyroscopeSensor = createSensorBlock('Gyroscope', Gyroscope);
+const AccelerometerSensor = createSensorBlock('Accelerometer', Accelerometer);
+const MagnetometerSensor = createSensorBlock('Magnetometer', Magnetometer);
+const MagnetometerUncalibratedSensor = createSensorBlock(
+  'Magnetometer (Uncalibrated)',
+  MagnetometerUncalibrated
+);
 
 function round(n) {
   if (!n) {
